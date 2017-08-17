1844a623434bf8ee75d6bf334952e0e4481b1f00
Keran Han <keran.han@gmail.com>
2017-08-16 19:18:51 -0500

    add old code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driverPeriod.java
@@ -0,0 +1,171 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+
+
+
+@TeleOp(name="Driver", group = "TeleOp")
+//@Disabled
+public class driverPeriod extends OpMode {
+
+    DcMotorController controllerFR;
+    DcMotorController controllerI;
+
+    int motorBR = 1;
+    int motorBL = 2;
+    int motorIntake = 1;
+    int motorShooter = 2;
+    int EncoderTarget = 0;
+    int timeout = 1;
+
+
+   /*DigitalChannel limit;
+   DigitalChannel limit1;
+   boolean limitState;
+   boolean limitState1;*/
+
+    ElapsedTime runtime = new ElapsedTime();
+
+
+    //VuforiaSensor vuforia = new VuforiaSensor(0,1,0);
+
+    public driverPeriod() {
+    }
+
+    @Override
+    public void init() {
+        controllerFR = hardwareMap.dcMotorController.get("controller1");
+        controllerI = hardwareMap.dcMotorController.get("controller2");
+        controllerI.setMotorMode(motorShooter,DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        controllerI.setMotorMode(motorShooter,DcMotor.RunMode.RUN_USING_ENCODER);
+
+       /*limit = hardwareMap.digitalChannel.get("limit");
+       limit1 = hardwareMap.digitalChannel.get("limit1");*/
+    }
+
+    @Override
+    public void loop() {
+
+
+        //Left Motors
+        float left = gamepad1.left_stick_y;
+
+        left = Range.clip(left, -1, 1);
+
+        left = (float) scaleInput(left);
+
+        //Right Motors
+        float right = -gamepad1.right_stick_y;
+
+        right = Range.clip(right, -1, 1);
+
+        right = (float) scaleInput(right);
+
+        //Motor Powers
+        controllerFR.setMotorPower(motorBR, -right);
+        controllerFR.setMotorPower(motorBL, -left);
+
+
+        //Limit Switch
+       /*limitState = limit.getState();
+       limitState1 = limit1.getState();*/
+
+       /*telemetry.addData("LimitState", limitState);
+       telemetry.addData("LimitState1", limitState1);*/
+
+
+        //Intake
+        if (gamepad2.right_trigger > 0.2) {
+            controllerI.setMotorPower(motorIntake, .5);
+        }
+        else if(gamepad2.left_trigger > 0.2)
+        {
+            controllerI.setMotorPower(motorIntake, -.5);
+        }
+        else
+        {
+            controllerI.setMotorPower(motorIntake, 0.0);
+        }
+
+        if (gamepad2.a) {
+            ElapsedTime timer = new ElapsedTime();
+            int target = controllerI.getMotorCurrentPosition(motorShooter) + (-1050);
+            controllerI.setMotorTargetPosition(motorShooter, target);
+
+            controllerI.setMotorMode(motorShooter, DcMotor.RunMode.RUN_TO_POSITION);
+            controllerI.setMotorPower(motorShooter, 1.0);
+            timer.reset();
+            while (controllerI.isBusy(motorShooter) && timer.seconds() < 1){
+               /*if (limitState == true){
+                   controllerI.setMotorPower(motorShooter, 0);
+               }
+               if (limitState1 == false){
+                   controllerI.setMotorPower(motorShooter, 0);
+               }*/
+            }
+            controllerI.setMotorPower(motorShooter, 0.0);
+            controllerI.setMotorMode(motorShooter,DcMotor.RunMode.RUN_USING_ENCODER);
+        }
+        else if(gamepad2.dpad_down)
+        {
+            controllerI.setMotorPower(motorShooter, -.3);
+        }
+        else if(gamepad2.dpad_up)
+        {
+            controllerI.setMotorPower(motorShooter, .3);
+        }
+        else
+        {
+            controllerI.setMotorPower(motorShooter, 0.0);
+        }
+
+
+        telemetry.update();
+
+        telemetry.addData("1 Text", "*** Robot Data***");
+        telemetry.addData("2 Motor Power Left", String.format("%.2f", left));
+        telemetry.addData("3 Motor Power Right", String.format("%.2f", right));
+
+    }
+
+
+    @Override
+    public void stop() {
+
+    }
+
+    private double scaleInput(double dVal)  {
+        double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
+                0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
+
+        // get the corresponding index for the scaleInput array.
+        int index = (int) (dVal * 16.0);
+        if (index < 0) {
+            index = -index;
+        } else if (index > 16) {
+            index = 16;
+        }
+
+        double dScale = 0.0;
+        if (dVal < 0) {
+            dScale = -scaleArray[index];
+        } else {
+            dScale = scaleArray[index];
+        }
+
+        return dScale;
+    }
+
+}
+
+
+
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vuforiaOp.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.vuforia.HINT;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
+/**
+ * Created by abrah on 10/14/2016.
+ */
+@Autonomous(name="VufOp", group="vuf")
+@Disabled
+public class vuforiaOp extends LinearOpMode {
+
+    public void runOpMode() throws InterruptedException {
+        VuforiaLocalizer.Parameters params = new VuforiaLocalizer.Parameters(R.id.cameraMonitorViewId);
+        params.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        params.vuforiaLicenseKey = "AdksQ3j/////AAAAGVB9GUsSEE0BlMaVB7HcRZRM4Sv74bxusFbCpn3gwnUkr3GuOtSWhrTCHnTU/93+Im+JlrYI6///bytu1igZT48xQ6182nSTpVzJ2ZP+Q/sNzSg3qvIOMnjEptutngqB+e3mQ1+YTiDa9aZod1e8X7UvGsAJ3cfV+X/S3E4M/81d1IRSMPRPEaLpKFdMqN3AcbDpBHoqp82fAp7XWVN3qd/BRe0CAAoNsr26scPBAxvm9cizRG1WeRSFms3XkwFN6eGpH7VpNAdPPXep9RQ3lLZMTFQGOfiV/vRQXq/Tlaj/b7dkA12zBSW81MfBiXRxp06NGieFe7KvXNuu2aDyyXoaPFsI44FEGp1z/SVSEVR4";
+
+        params.cameraMonitorFeedback = VuforiaLocalizer.Parameters.CameraMonitorFeedback.AXES;
+
+        VuforiaLocalizer vuforia = ClassFactory.createVuforiaLocalizer(params);
+
+
+        VuforiaTrackables beacons = vuforia.loadTrackablesFromAsset("FTC_2016-17");
+        beacons.get(0).setName("Wheels");
+        beacons.get(1).setName("Tools");
+        beacons.get(2).setName("Lego");
+        beacons.get(3).setName("Gears");
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            for (VuforiaTrackable beac : beacons) {
+                OpenGLMatrix pose = ((VuforiaTrackableDefaultListener) beac.getListener()).getPose();
+
+                if (pose != null) {
+                    VectorF translation = pose.getTranslation();
+
+                    telemetry.addData(beac.getName() + "-Translation", translation);
+
+                    double degreesToTurn = Math.toDegrees(Math.atan2(translation.get(1), translation.get(2))); //vertical phone. for landscape phone translation.get(0), translation.get(2)
+
+                    telemetry.addData(beac.getName() + "-Degrees", degreesToTurn);
+                }
+            }
+            telemetry.update();
+        }
+    }
+}
+
